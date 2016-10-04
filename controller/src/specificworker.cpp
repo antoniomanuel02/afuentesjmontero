1b042bbb11d2b4ad01a5e404fc56834afb4b50b6
agarciatu <agarciatu@alumnos.unex.es>
2016-10-04 17:18:49 +0200

    Version 1.4 - El robot no toca las cajas

--- controller/src/specificworker.cpp
+++ controller/src/specificworker.cpp
@@ -48,19 +48,34 @@ bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
 
 void SpecificWorker::compute()
 {
-    const float threshold = 200; //millimeters
+    const float threshold = 400; //millimeters
     float rot = 0.6;  //rads per second
-
+    
     try
     {
+        
         RoboCompLaser::TLaserData ldata = laser_proxy->getLaserData();  //read laser data 
         std::sort( ldata.begin() +5, ldata.end()-5, [](RoboCompLaser::TData a, RoboCompLaser::TData b){ return     a.dist < b.dist; }) ;  //sort laser data from small to large distances using a lambda function.
 
-    if( ldata[6].dist - 350 < threshold)//ldata.front().dist < threshold)
+    if( ldata[6].dist  < threshold)//ldata.front().dist < threshold)
     {
-        std::cout << ldata.front().dist << std::endl;
-        differentialrobot_proxy->setSpeedBase(5, rot);
-        usleep(rand()%(1500000-100000 + 1) + 100000);  //random wait between 1.5s and 0.1sec
+      if(ldata[6].angle > 0) 
+      {
+	if(ldata[6].angle < 45){
+	  std::cout << ldata.front().dist << std::endl;
+	  differentialrobot_proxy->setSpeedBase(5, -rot);
+	  usleep(rand()%(1500000-100000 + 1) + 100000);  //random wait between 1.5s and 0.1sec
+	}
+	
+      }else {
+	if(ldata[6].angle > -45){
+	  std::cout << ldata.front().dist << std::endl;
+	  differentialrobot_proxy->setSpeedBase(5, rot);
+	  usleep(rand()%(1500000-100000 + 1) + 100000);  //random wait between 1.5s and 0.1se
+	}
+	  
+      }
+      
     }
     else
     {
